767dd01f14e3965369d539c072a9ddb125ab6414
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2019-01-22 14:38:59 +0100

    Storage Listeners
    Писал Listeners за диск и тд)

---
+++ Storage/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using unwdmi.Protobuf;
+
+namespace unwdmi.Storage
+{
+    class Controller
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+
+            while (true)
+            {
+                var _string = Console.ReadLine();
+                if (_string.ToLower() == "suka")
+                {
+                    Console.WriteLine("BLYAT");
+                }
+            }
+
+
+        }
+
+        public ListenerParser ListenerParser;
+        public ListenerWeb ListenerWeb;
+        public static Controller Instance;
+
+        public Controller()
+        {
+            Instance = this;
+            ListenerParser = new ListenerParser(this);
+            ListenerWeb = new ListenerWeb(this);
+        }
+    }
+}
---
+++ Storage/Listener.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace unwdmi.Storage
+{
+    class ListenerParser
+    {
+        public ListenerParser(Controller instance)
+        {
+            _controller = instance;
+        }
+
+        private readonly Controller _controller;
+
+        private Socket _listener;
+
+        public void StartListening()
+        {
+            var minecraft = 25565;
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, minecraft);
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _listener.Blocking = false;
+            _listener.Bind(ip);
+            _listener.Listen(200);
+            _listener.BeginAccept(new AsyncCallback(AcceptCallBack), _listener);
+        }
+
+
+        void AcceptCallBack(IAsyncResult ar)
+        {
+            // Program can act weird if all 800 connections are opened at once.
+            // In real-life applications this shouldn't be a problem.
+            _listener.BeginAccept(new AsyncCallback(AcceptCallBack), _listener);
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject stateObject = new StateObject
+            {
+                workSocket = handler
+            };
+            handler.BeginReceive(stateObject.buffer, 0, StateObject.BUFFER_SIZE, 0, ReceiveCallback, stateObject);
+        }
+
+        void ReceiveCallback(IAsyncResult ar)
+        {
+            var so = (StateObject)ar.AsyncState;
+            
+        }
+    }
+
+    class ListenerWeb
+    {
+        public ListenerWeb(Controller instance)
+        {
+            _controller = instance;
+        }
+
+        private readonly Controller _controller;
+
+        private Socket _listener;
+
+        public void StartListening()
+        {
+            var minecraft = 25566;
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, minecraft);
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _listener.Blocking = false;
+            _listener.Bind(ip);
+            _listener.Listen(200);
+            _listener.BeginAccept(new AsyncCallback(AcceptCallBack), _listener);
+        }
+
+
+        void AcceptCallBack(IAsyncResult ar)
+        {
+            // Program can act weird if all 800 connections are opened at once.
+            // In real-life applications this shouldn't be a problem.
+            _listener.BeginAccept(new AsyncCallback(AcceptCallBack), _listener);
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject stateObject = new StateObject
+            {
+                workSocket = handler
+            };
+            handler.BeginReceive(stateObject.buffer, 0, StateObject.BUFFER_SIZE, 0, ReceiveCallback, stateObject);
+        }
+
+        void ReceiveCallback(IAsyncResult ar)
+        {
+            var so = (StateObject)ar.AsyncState;
+
+        }
+    }
+
+    public class StateObject
+    {
+        public Socket workSocket;
+        // 4096 fits all the XML files, so the checks don't have to be done as often, saves a bit of CPU, costs a bit more ram.
+        public const int BUFFER_SIZE = 4096;
+        public byte[] buffer = new byte[BUFFER_SIZE];
+        public StringBuilder sb = new StringBuilder();
+    }
+}
