ff69610f553b1681b71b93de2509a9b7cbb2da57
Stijn <p.stijnberend@hotmail.com>
2019-01-28 21:18:50 +0100

    Updated some shit
    haat aan django, doet fucking kut over alles

--- Django Team Daddy 2-2/teamdaddy22/homepage/urls.py
+++ Django Team Daddy 2-2/teamdaddy22/homepage/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('data', views.data, name='data'),
 ]
--- Django Team Daddy 2-2/teamdaddy22/homepage/views.py
+++ Django Team Daddy 2-2/teamdaddy22/homepage/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 import time
 
 def index(request):
-	return render(request=request, template_name="homepage/home.html", context={"date": time.strftime("%H:%M:%S")})
+	return render(request=request, template_name="homepage/login.html", context={"date": time.strftime("%H:%M:%S")})
 	# HttpResponse("<style>body{background:#3c3c3c;color:#fff;font-size:3em}h1{margin:0;}div{ margin:0; position:absolute; left:50vw; top:50vh; transform:translate(-50%, -50%); text-align:center; line-height:.6 }strong{color:#17a2b8}</style><div><h1>Hello world</h1></br>A <strong>beautiful</strong> world</div>")
 # Create your views here.
+
+def data(request):
+	return render(request=request, template_name="homepage/home.html", context={})
